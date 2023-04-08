50a6ac934d9441afbe747c2af953c1598e69a986
Elisabeth ROBERT AOUCHER <elisabeth.robert.aoucher@gmail.com>
2023-04-08 18:47:25 +0200

    ma premiere page2

--- src/Controller/HiController.php
+++ src/Controller/HiController.php
@@ -19,19 +19,42 @@ use Symfony\Component\Routing\Annotation\Route;
 /* la class hi controller que l'on a créer étend de la class AbstractController
 parce que dans le abstract il y énormement d'outils que 'on va pouvoir réutiliser dans le notre
 
+Si on enlève le Abstract la méthode render n'existerait plus car notre classe n'a pas de méthode render qui a été définis
+La méthode render retourne une réponse de type http fundation
+
 */
 
 class HiController extends AbstractController
 {
+    /** #[Route] c'est un attribut permet d'associer l'URI à la fonction qui va être exécuter.µ
+     * Lorsque je veux éxécuter une fonction à travers une requete HTTP je dois lui associer une route
+     * On peut aussi écrire d'une ancienne façon en utilisant les annotations maintenant on utilise les attributs.Le
+     * name va être l'identifiant de la route /hi on ne doit pas avoir deux noms identiques pour deux routes
+      */
     #[Route('/hi', name: 'app_hi')]
 
     /* dans cette fonction il retourne notre vue twig qu'il a créer dans le
-    terminal grâce à la console*/
+    terminal grâce à la console la fonction retourne une Réponse car Symfony c'est un framework Request/Response
+    car si je reçois une réponse qui est /hi je voudrais répondre avec cette fonction là qui elle va me retourner à la fin une
+    réponse 
+    
+    */
 
     public function index(): Response
     {
-        return $this->render('hi/index.html.twig', [
-            'controller_name' => 'HiController',
-        ]);
+        // return $this->render('hi/index.html.twig', [
+        //     'controller_name' => 'HiController',
+        // ]);
+
+        return new Response(
+            content:"
+            <head>
+                <title>Ma premiere page</title>
+            </head>
+            <body>
+                <h1>Ma premiere page</h1>
+            </body>
+            "
+        );
     }
 }
